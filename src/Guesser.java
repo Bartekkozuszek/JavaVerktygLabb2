177858200468726308053fc925434a631f559c2a
Bartek Kozuszek <bartekkozuszek@gmail.com>
2018-11-26 16:52:34 +0100

    Implemented hints in task 1-3

--- src/Guesser.java
+++ src/Guesser.java
@@ -63,12 +63,15 @@ public class Guesser{
     private String getReply(){
         Scanner sc = new Scanner(System.in);
         String reply = null;
-        do {
-            System.out.println("Please answer T for true or F for false\n");
-            reply = sc.nextLine();
 
+        System.out.println("Please answer T for true or F for false\n");
+        reply = sc.nextLine();
+
+
+        while (!"T".equals(reply) && !"F".equals(reply)){
+            System.out.println("Please answer T or F");
+            reply = sc.nextLine();
         }
-        while (!reply.equals("T") && !reply.equals("F"));
         // Write code here which reads a String from the console.
         // As long as it is not a valid reply (one of "T" and "F")
         // write an error message, and read a new reply.
--- src/TestGuessser.java
+++ src/TestGuessser.java
@@ -3,6 +3,7 @@ public class TestGuessser {
     public static void main(String []args){
 
         Guesser guesser = new Guesser(0,10);
+
         if(guesser.toString().equals("low: 0 high: 10")){
             System.out.println("Test with 0, 10 passed!");
         }else{
